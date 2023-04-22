f3aaa8a724907e834b5d4aa19143062479953ca4
KarenSSanchezP <sp21013@ues.edu.sv>
2023-04-22 13:23:44 -0600

    Configurando algunas cosas...

--- Sesion7B/AerolineaProject/AerolineaProject/settings.py
+++ Sesion7B/AerolineaProject/AerolineaProject/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qwj7%ja%l_ki(^mat=h+ldr=vi8c8mrgvgex9)%wa)$xp(o9iq'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
